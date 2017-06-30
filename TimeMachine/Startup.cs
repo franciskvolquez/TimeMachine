f30e870342a8d646037234dc44ba22a843981d59
Isabad Martinez <isabad_martinez@claro.com.do>
2017-06-30 10:22:42 -0400

    Add project files.

---
+++ TimeMachine/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TimeMachine.Startup))]
+namespace TimeMachine
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
